b6415c4036e70ec35e7c889005e413be5dee8545
sota1235 <sota1235@gmail.com>
2016-11-09 15:06:18 +0900

    add: config for webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Config for webpack.
+ */
+const path = require('path');
+const glob = require('glob');
+
+const jsBasePath = path.resolve(__dirname, 'src');
+const targetDirectories = ['pc', 'sp'];
+
+const targets = glob.sync(`${jsBasePath}/+(${targetDirectories.join('|')})/*.js`);
+const entries = {};
+
+targets.forEach(value => {
+  const re = new RegExp(`${jsBasePath}/`);
+  const key = value.replace(re, '')+
+  entries[key] = value;
+});
+
+module.exports = {
+  entry: entries,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]',
+  },
+};
